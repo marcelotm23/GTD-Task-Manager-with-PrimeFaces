2a45f3e7e3b83188f9bff23a93001aafafe959d5
marcelotm23 <marcelo.torrejn@gmail.com>
2017-03-17 20:56:35 +0100

    Filtro terminado :+1:

--- sdi2-28/src/com/sdi/presentation/filters/LoginFilter.java
+++ sdi2-28/src/com/sdi/presentation/filters/LoginFilter.java
@@ -1,6 +1,7 @@
 package com.sdi.presentation.filters;
 
 import java.io.IOException;
+
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -10,25 +11,27 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet Filter implementation class LoginFilter
  */
-@WebFilter(
-		dispatcherTypes = {DispatcherType.REQUEST }
-					, 
-		urlPatterns = { "/restricted/*" }, 
-		initParams = { 
-				@WebInitParam(name = "LoginParam", value = "/index.html")
-		})
+@WebFilter(dispatcherTypes = { DispatcherType.REQUEST }, urlPatterns = { "/restricted/*" }, initParams = { @WebInitParam(name = "LoginParam", value = "/index.xhtml") })
 public class LoginFilter implements Filter {
 
-    /**
-     * Default constructor. 
-     */
-    public LoginFilter() {
-        // TODO Auto-generated constructor stub
-    }
+	// Necesitamos acceder a los parámetros de inicialización en
+	// el método doFilter por lo que necesitamos la variable
+	// config que se inicializará en init()
+	FilterConfig config = null;
+
+	/**
+	 * Default constructor.
+	 */
+	public LoginFilter() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see Filter#destroy()
@@ -40,11 +43,30 @@ public class LoginFilter implements Filter {
 	/**
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		// Si no es petición HTTP nada que hacer
+		if (!(request instanceof HttpServletRequest)) {
 
-		// pass the request along the filter chain
+			chain.doFilter(request, response);
+
+			return;
+		}
+		// En el resto de casos se verifica que se haya hecho login previamente
+		HttpServletRequest req = (HttpServletRequest) request;
+
+		HttpServletResponse res = (HttpServletResponse) response;
+		HttpSession session = req.getSession();
+		if (session.getAttribute("LOGGEDIN_USER") == null) {
+
+			String loginForm = config.getInitParameter("LoginParam");
+
+			// Si no hay login, redirección al formulario de login
+
+			res.sendRedirect(req.getContextPath() + loginForm);
+
+			return;
+		}
 		chain.doFilter(request, response);
 	}
 
@@ -52,7 +74,8 @@ public class LoginFilter implements Filter {
 	 * @see Filter#init(FilterConfig)
 	 */
 	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
+		// Iniciamos la variable de instancia config
+		config = fConfig;
 	}
 
 }
